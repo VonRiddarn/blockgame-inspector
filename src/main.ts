5c725033c4a5338bba3b8624c90e0ecc1ce0b043
VonRiddarn <vonriddarn@gmail.com>
2024-12-17 15:25:18 +0100

    fix: switched api to playerdb

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import UserInfo from "./UserInfo";
 
 // Operational
 let nameInputEl = document.getElementById("input-playername") as HTMLInputElement;
+nameInputEl.value = "VonRiddarn";
 let clothesEl = document.getElementById("input-clothes") as HTMLInputElement;
+clothesEl.checked = true;
 let inspectButtonEl = document.getElementById("input-inspect") as HTMLElement;
 
 // Graphical
@@ -11,7 +13,7 @@ let uuidEl = document.getElementById("uuid") as HTMLElement;
 let showcaseEl = document.getElementById("showcase") as HTMLElement;
 
 inspectButtonEl.addEventListener('click', async () => {
-	const response = await fetch(`https://api.mojang.com/users/profiles/minecraft/${nameInputEl.value}`);
+	const response = await fetch(`https://playerdb.co/api/player/minecraft/${nameInputEl.value}`);
 	const userInfo = await response.json();
 
 	buildImage(userInfo as unknown as UserInfo);
@@ -19,18 +21,14 @@ inspectButtonEl.addEventListener('click', async () => {
 
 const buildImage = (userInfo:UserInfo) => {
 
-	if(userInfo["errorMessage"] != undefined)
+	if(!userInfo.data.player)
 	{
-		uuidEl.innerHTML = "USER WAS NOT FOUND";
+		uuidEl.innerHTML = "Player does not exist!";
 		return;
 	}
 
-	// A little hacky as we promised a name.
-	// Had we not error checked above this would go to hell
-	aliasEl.innerHTML = userInfo.name;
-	uuidEl.innerHTML = userInfo.id;
-
 	let url = "https://crafatar.com/renders/body/";
-	let arg:string = clothesEl.checked ? "overlay=true" : ""; // The api does not accept a false argument for some reason
-	showcaseEl.setAttribute("src", `${url}${userInfo.id}${arg}`);
+	let arg:string = clothesEl.checked ? "overlay" : ""; // The api does not accept a false argument for some reason
+	showcaseEl.setAttribute("src", `${url}${userInfo.data.player.raw_id}?${arg}&scale=10&size=512`);
+	aliasEl.innerHTML = userInfo.data.player.username;
 };
